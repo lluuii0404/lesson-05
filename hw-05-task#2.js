8b41440e8cb3f3096f092d33207bc88d45ebb83e
lluuii <u0859753@gmail.com>
2018-11-12 18:58:30 +0200

    change code

--- hw-05-task#2.js
+++ hw-05-task#2.js
@@ -1,5 +1,5 @@
 var style = document.head.appendChild(
-	document.createElement("style")
+    document.createElement("style")
 )
 style.innerText = `
     .circle {
@@ -21,48 +21,48 @@ style.innerText = `
         font-size: 16px;
         text-align: center;
     }
-	div.circle {
-		margin: 0 auto;
-		background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVsBS5O1u08rbDJiOOIqDY8TiiNfrk4wlh_C3Oxm5zVgPAPO03vQ");
-		background-position: center;
+    div.circle {
+        margin: 0 auto;
+        background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVsBS5O1u08rbDJiOOIqDY8TiiNfrk4wlh_C3Oxm5zVgPAPO03vQ");
+        background-position: center;
         background-repeat: no-repeat;
         background-size: cover;
-	}
-	img.circle{
-		margin: 1em;
-		display: inline-block;
-	}
-	.container {
-		width: 800px;
-		margin: 0 auto;
-	}
+    }
+    img.circle{
+        margin: 1em;
+        display: inline-block;
+    }
+    .container {
+        width: 800px;
+        margin: 0 auto;
+    }
 `
 
 var tags = [
-	{
+    {
         name: "div",
         attrs: { 
-		className: "circle",
-	},
-	children: [
+                className: "circle",
+    },
+    children: [
             {
                 name: "h1", 
                 attrs: { 
-					className: "header" 
-				},
+                                    className: "header" 
+                                },
                 text: "Nami"
             }
-    	]
+        ]
     },
     {
         name: "p", 
         attrs: { 
-			className: "paragraph" 
-		},
+                className: "paragraph" 
+        },
         text: "Hello, My name is Nami. I'm cat. Look and click on my photo =)"
     },
-	{
-    	name: "div",
+    {
+        name: "div",
         attrs: {
             className: "container",
         },
@@ -70,28 +70,28 @@ var tags = [
             {
                 name: "img",
                 attrs: {
-                	src: "http://vsezhivotnye.com/sites/default/files/imagecache/resizeimgpost-500-500/u69/2012/06/1313317041_239312872_3----.jpg",
+                    src: "http://vsezhivotnye.com/sites/default/files/imagecache/resizeimgpost-500-500/u69/2012/06/1313317041_239312872_3----.jpg",
                     className: "circle"
                 }
             },
-			{
+            {
                 name: "img",
                 attrs: {
-                	src: "http://multipet.ru/images/image2breed/britanskaya1.b1.jpg",
+                    src: "http://multipet.ru/images/image2breed/britanskaya1.b1.jpg",
                     className: "circle"
                 }
             },
-			{
+            {
                 name: "img",
                 attrs: {
-                	src: "https://zooclub.ru/attach/10000/10990.jpg",
+                    src: "https://zooclub.ru/attach/10000/10990.jpg",
                     className: "circle"
                 }
             },
-			{
+            {
                 name: "img",
                 attrs: {
-                	src: "http://www.sunny-cat.ru/datas/users/1-agidel_design001.jpg",
+                    src: "http://www.sunny-cat.ru/datas/users/1-agidel_design001.jpg",
                     className: "circle"
                 }
             }
@@ -101,20 +101,20 @@ var tags = [
 ]
 
 function addElement ( elemCh, elemParent ) {
-	var elem = elemParent.appendChild ( 
-		document.createElement ( elemCh.name ) 
-	)
+    var elem = elemParent.appendChild ( 
+        document.createElement ( elemCh.name ) 
+    )
     if ( elemCh.text ) 
-		elem.innerHTML = elemCh.text
-   	for ( var attr in elemCh.attrs )  
-		elem [ attr ] = elemCh.attrs [ attr ]
+        elem.innerHTML = elemCh.text
+    for ( var attr in elemCh.attrs )  
+        elem [ attr ] = elemCh.attrs [ attr ]
     return elem
 }
 for ( var tag of tags ) {
     var elem = addElement ( tag, document.body )
     if ( !tag.children ) continue
     for ( var child of tag.children )  
-		addElement ( child, elem )
+        addElement ( child, elem )
 }
 
 var collection = document.getElementsByClassName ( "circle" )
@@ -122,84 +122,85 @@ var collection = document.getElementsByClassName ( "circle" )
 console.log("Collection:= ", collection )
 
 var str = document.body.appendChild(
-	document.createElement("p")
+    document.createElement("p")
 )
 var functionArray = [
-    function (){
-		str.innerText = "it's my avatar"
-		str.style = `
-			font-family: monospace, Arial;
-			color: blue;
-			font-size: 16px;
-			text-align: center;
-		`
+    function (event){
+        event.stopImmediatePropagation()
+        str.innerText = "it's my avatar"
+        str.style = `
+            font-family: monospace, Arial;
+            color: blue;
+            font-size: 16px;
+            text-align: center;
+        `
     },
-    function (){
-		str.innerText = "it's me in childhood"
-		str.style = `
-			font-family: monospace, Arial;
-			color: red;
-			font-size: 16px;
-			text-align: center;
-		`
+    function (event){
+        str.innerText = "it's me in childhood"
+        str.style = `
+            font-family: monospace, Arial;
+            color: red;
+            font-size: 16px;
+            text-align: center;
+        `
     },
-    function (){
-		str.innerText = "look i'm on the hunt"
-		str.style = `
-			font-family: monospace, Arial;
-			color: green;
-			font-size: 16px;
-			text-align: center;
-		`
+    function (event){
+        str.innerText = "look i'm on the hunt"
+        str.style = `
+            font-family: monospace, Arial;
+            color: green;
+            font-size: 16px;
+            text-align: center;
+        `
     },
-    function (){
-		str.innerText = "it means 'what do you give me yummy?'"
-		str.style = `
-			font-family: monospace, Arial;
-			color: orange;
-			font-size: 16px;
-			text-align: center;
-		`
+    function (event){
+        str.innerText = "it means 'what do you give me yummy?'"
+        str.style = `
+            font-family: monospace, Arial;
+            color: orange;
+            font-size: 16px;
+            text-align: center;
+        `
     },
-    function (){
-		str.innerText = "what?"
-		str.style = `
-			font-family: monospace, Arial;
-			color: pink;
-			font-size: 16px;
-			text-align: center;
-		`
+    function (event){
+        str.innerText = "what?"
+        str.style = `
+            font-family: monospace, Arial;
+            color: pink;
+            font-size: 16px;
+            text-align: center;
+        `
     }
 ]
 
 function clickHandler (event) {
-	//event.stopPropagation()
-	this.style = `
-    	width: 200px;
-        height: 200px;
+    //event.stopPropagation()
+    event.stopImmediatePropagation()
+    this.style = `
+        width: 200px;
+      height: 200px;
         transition: all .5s;
-		border: 3px solid silver;
+            border: 3px solid silver;
     `
 }
 
 
 var ind = 0
 for ( var elemColl of collection ) {
-	elemColl.onclick = functionArray[ind++]
-    elemColl.addEventListener ( 'click', clickHandler )
-	
-	elemColl.onmouseover = function(event) {
-		this.style = `
+    elemColl.onclick = functionArray[ind++]
+  elemColl.addEventListener ( 'click', clickHandler )
+    
+    elemColl.onmouseover = function(event) {
+        this.style = `
             border: 3px solid silver;
         `
-	}
-	elemColl.onmouseout = function(event){
+    }
+    elemColl.onmouseout = function(event){
         this.style = `
             width: 150px;
             height: 150px;
             transition: all .5s;
         `
-		str.innerText = ''
+        str.innerText = ''
     }
-	
 }
